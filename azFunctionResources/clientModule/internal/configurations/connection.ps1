6f79f4e253452036efd8696a5cb25ccd526c9a0b
Matthew Johnson <mjohnson@mwjcomputing.com>
2024-03-20 10:01:30 -0400

    Initial Commit

---
+++ azFunctionResources/clientModule/internal/configurations/connection.ps1
@@ -0,0 +1,3 @@
+﻿Set-PSFConfig -Module 'PoshCISAKevFeed' -Name 'Client.Uri' -Value $null -Initialize -Validation 'string' -Description "Url to connect to the PoshCISAKevFeed Azure function"
+Set-PSFConfig -Module 'PoshCISAKevFeed' -Name 'Client.UnprotectedToken' -Value '' -Initialize -Validation 'string' -Description "The unencrypted access token to the PoshCISAKevFeed Azure function. ONLY use this from secure locations or non-sensitive functions!"
+Set-PSFConfig -Module 'PoshCISAKevFeed' -Name 'Client.ProtectedToken' -Value $null -Initialize -Validation 'credential' -Description "An encrypted access token to the PoshCISAKevFeed Azure function. Use this to persist an access token in a way only the current user on the current system can access."
